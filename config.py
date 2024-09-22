54a09ac47b5556bb16bd2ca018dc5d1046d564df
bobpot <BRPOTHURI@GMAIL.COM>
2024-09-22 02:05:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config:
     #
     color_themes = {
         "dark_modern": {
-            "hallway": pygame.Color(137, 190, 209),
+            "hallway": pygame.Color(137, 134, 255),
             "background": pygame.Color(24, 26, 30),
             "square": [
                 pygame.Color(224, 26, 79),
